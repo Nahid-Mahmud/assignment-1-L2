f74bd5abb212a2da351c1aeedf33f51abfa78903
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-05-08 16:40:04 +0600

    Implement concatenateArrays function and update tsconfig.json formatting

--- index.ts
+++ index.ts
@@ -5,8 +5,24 @@ function formatString(input: string, toUpper?: boolean): string {
   return input.toLowerCase();
 }
 
-
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
   const filteredItems = items?.filter((item: { title: string; rating: number }) => item?.rating >= 4);
   return filteredItems;
+}
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  const arrayLength = arrays.length;
+
+  let convertedArray = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    const firstLevelArrayLength: number = arrays[i].length;
+    const firstLevelArray = arrays[i];
+
+    for (let j = 0; j < firstLevelArrayLength; j++) {
+      convertedArray.push(firstLevelArray[j]);
+    }
+  }
+
+  return convertedArray;
 }
